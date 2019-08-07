1bb15aef0b321cbad5d07b81a78510fbb5f83161
Sergei Mikhailov <saboteurincave@gmail.com>
2019-08-07 19:45:24 +0300

    novice: prime numbers (hard), 10-to-x and x-to-10 - number scale migrations

---
+++ Novice/Loop/x-to-10/generators/generator.py
@@ -0,0 +1,28 @@
+import random
+import sys
+
+
+random.seed(sys.argv[1])
+
+def convert_base(num, to_base=10, from_base=10):
+    # first convert to decimal number
+    if isinstance(num, str):
+        n = int(num, from_base)
+    else:
+        n = int(num)
+    # now convert decimal to 'to_base' base
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if n < to_base:
+        return alphabet[n]
+    else:
+        return convert_base(n // to_base, to_base) + alphabet[n % to_base]
+
+
+if sys.argv[1] == '0':
+    print('100 2')
+elif sys.argv[1] == '1':
+    print('A 16')
+else:
+    scale = random.randint(2, 16)
+    number = random.randint(1, 10000)
+    print('{} {}'.format(convert_base(number, scale), scale))
