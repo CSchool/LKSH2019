a076a1829e279fd036261e503a4e4d6b25ab6bbd
Sergei Mikhailov <saboteurincave@gmail.com>
2019-08-08 10:22:35 +0300

    fix prime numbers

--- Novice/Loop/prime-number-hard/solutions/main.cpp
+++ Novice/Loop/prime-number-hard/solutions/main.cpp
@@ -15,13 +15,14 @@ int main()
 
     for (int number = 2; number < a; ++ number) {
         bool isPrime = true;
-        double limit = ceil(sqrt(number)) + 1;
+        double limit = floor(sqrt(number)) + 1;
         for (long int divider = 2; divider < limit; ++divider) {
             if (number % divider == 0) {
                 isPrime = false;
                 break;
             }
         }
+
 
         if (isPrime) {
             cout << number << " ";
